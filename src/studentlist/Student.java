31682e5747935f07cda5cc2daf1b28adeb163847
espinogi <espinogi@sheridancollege.ca>
2024-09-10 18:08:46 -0400

    studentlist version 1

---
+++ src/studentlist/Student.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author giancarloespinosa
+ */
+public class Student { // model the student raw data 
+    
+    private String name; // variables private
+    private int sid; // principle "encapsulation"
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+    
+}
