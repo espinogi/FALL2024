25c4c5d874b3073590fd8b3fcfe607a91140f33b
espinogi <espinogi@sheridancollege.ca>
2024-09-17 16:45:03 -0400

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -13,6 +13,8 @@ public class Student { // model the student raw data
     private String name; // variables private
     private int sid; // principle "encapsulation"
 
+    private float fee;
+
     /**
      * @return the name
      */
